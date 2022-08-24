0adcf3cd20d0bae3d51dc889a5813956b3bab70a
Edgard_Roul <edgardo_raul@hotmail.com>
2022-08-24 12:41:11 -0300

    Se borran más cosas

--- wp-content/themes/understrap/dist/vendor/wptrt/control-color-alpha/src/index.js
+++
@@ -1,6 +0,0 @@
-/* global wp */
-
-import WPTRTColorAlphaControl from './WPTRTColorAlphaControl';
-
-// Register control type with Customizer.
-wp.customize.controlConstructor['color-alpha'] = WPTRTColorAlphaControl;
